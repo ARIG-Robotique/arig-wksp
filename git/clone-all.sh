2fd45c87507a9be00471315bdb67d5df77e3383b
Gregory DEPUILLE <gregory.depuille@sogelink.fr>
2015-03-30 19:02:24 +0200

    Ajout commentaire + fritzing parts + java

--- git/clone-all.sh
+++ git/clone-all.sh
@@ -17,10 +17,14 @@ fi
 ROOT_DIR=`pwd`
 CPP_DIR=cpp
 JAVA_DIR=java
+ELEC_DIR=electronique
 EXTERNAL_DIR=external
+
+###################################################
 
 logInfo "Projet C++"
 
+cd $ROOT_DIR
 mkdir $CPP_DIR
 cd $CPP_DIR
 
@@ -39,21 +43,48 @@ git clone git@github.com:ARIG-Robotique/robot-main-big.git
 logInfo "Récupération du programme du robot secondaire"
 git clone git@github.com:ARIG-Robotique/robot-main-small.git
 
+###################################################
+
+logInfo "Projet Java"
+
 cd $ROOT_DIR
+mkdir $JAVA_DIR
+
+logInfo "Récupération de la lib ARIG Robotique"
+git clone git@github.com:ARIG-Robotique/robot-system-lib-parent.git
+
+###################################################
+
+logInfo "Projet Electronique"
+
+cd $ROOT_DIR
+mkdir $ELEC_DIR
+
+logInfo "Récupération des parts Fritzing"
+git clone git@github.com:ARIG-Robotique/fritzing-parts.git
+
+###################################################
 
 logInfo "Récupération des dépendances externes"
 
+cd $ROOT_DIR
 mkdir -p $EXTERNAL_DIR/adafruit
 cd $EXTERNAL_DIR/adafruit
 
-git clone git@github.com:adafruit/Adafruit_Sensor.git
-git clone git@github.com:adafruit/Adafruit_10DOF.git
-git clone git@github.com:adafruit/Adafruit_BMP085_Unified.git
+logInfo "Dépendances Adafruit"
+
+logInfo "Dépendances pour l'écran LCD"
 git clone git@github.com:adafruit/Adafruit-GFX-Library.git
-git clone git@github.com:adafruit/Adafruit_L3GD20.git
+git clone git@github.com:adafruit/Adafruit_SSD1306.git
+
+logInfo "Dépendances pour le Gyroscope (10 DOF : BMP085, L3GD20, LSM303)"
+git clone git@github.com:adafruit/Adafruit_Sensor.git
+git clone git@github.com:adafruit/Adafruit_BMP085_Unified.git
 git clone git@github.com:adafruit/Adafruit_L3GD20_U.git
 git clone git@github.com:adafruit/Adafruit_LSM303DLHC.git
-git clone git@github.com:adafruit/Adafruit_SSD1306.git
+git clone git@github.com:adafruit/Adafruit_10DOF.git
+
+logInfo "Dépendances pour le capteur RGB"
 git clone git@github.com:adafruit/Adafruit_TCS34725.git
 
 cd $ROOT_DIR
