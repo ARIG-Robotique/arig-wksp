64d979a9f762382c3395e11cc961bf946f289e48
Gregory DEPUILLE <gregory.depuille@sogelink.fr>
2019-02-21 01:26:14 +0100

    Update CPP micro

--- cpp-micro/download.sh
+++ cpp-micro/download.sh
@@ -3,15 +3,17 @@
 ROOT_DIR=$(pwd)
 DOWNLOAD_DIR=/opt
 
-ARDUINO_SDK_VERSION=1.0.6
+ARDUINO_SDK_VERSION=1.8.8
 ARDUINO_FILENAME=arduino-${ARDUINO_SDK_VERSION}-linux64
-ARDUINO_DOWNLOAD_URL=https://downloads.arduino.cc/${ARDUINO_FILENAME}.tgz
+ARDUINO_FULL_FILENAME=${ARDUINO_FILENAME}.tar.xz
+ARDUINO_DOWNLOAD_URL=https://downloads.arduino.cc/${ARDUINO_FULL_FILENAME}
 
-cd $DOWNLOAD_DIR
-if [ ! -f "$ARDUINO_FILENAME.tgz" ] ; then
+echo "-- Download external dependencies"
+cd ${DOWNLOAD_DIR}
+if [[ ! -f "${ARDUINO_FULL_FILENAME}" ]] ; then
   echo "---- Download Arduino SDK $ARDUINO_SDK_VERSION ..."
-  curl -L $ARDUINO_DOWNLOAD_URL -o $ARDUINO_FILENAME.tgz
-  tar xvzf $ARDUINO_FILENAME.tgz
+  curl -L ${ARDUINO_DOWNLOAD_URL} -o ${ARDUINO_FULL_FILENAME}
+  tar xvJf ${ARDUINO_FULL_FILENAME}
+else
+  echo "---- Déjà téléchargé"
 fi
-
-chown -R root:root ${DOWNLOAD_DIR}/arduino-${ARDUINO_SDK_VERSION}
