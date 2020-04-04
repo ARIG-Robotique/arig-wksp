b3160fe1dfa85e787628df3c2abc88e1415609ed
Gregory DEPUILLE <gregory.depuille@sogelink.fr>
2020-04-04 17:36:25 +0200

    Avec un menu c'est mieux

--- javascript/run.sh
+++ javascript/run.sh
@@ -1,2 +1,49 @@
 #!/bin/bash
-for i in * ; do cd  ; yarn start & cd .. ; done
+
+CURRENT_DIR=$(pwd)
+START=0
+PIDS=()
+
+start() {
+	if [ ${START} -eq 1 ] ; then
+		echo "Already started !"
+		echo ""
+		return
+	fi
+
+	for p in $(ls -d */) ; do
+		cd ${p}
+		yarn start &
+		PIDS+=($!)
+		cd ..
+	done
+	START=1
+}
+
+quit() {
+	echo "Suppression des PIDs"
+	for pid in ${PIDS[@]} ; do
+		echo "Remove : ${pid}"
+		kill -9 ${pid}
+	done
+}
+
+mainmenu() {
+	echo "Press 's' to start"
+	echo "Press 'q' to quit"
+	read -n 1 -p "Key : " keyPressed
+	echo ""
+	if [ "$keyPressed" = "q" ]; then
+      quit
+  elif [ "$keyPressed" = "s" ]; then
+      start
+			mainmenu
+  else
+      echo "You have entered an invallid selection!"
+      echo "Please try again!"
+      echo ""
+      mainmenu
+  fi
+}
+
+mainmenu
