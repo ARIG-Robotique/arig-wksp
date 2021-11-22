bb0eee37ffa0837c8bc7b0d87b70d38a2ed817a5
Mathieu Percelay <mathieup@mailoo.org>
2021-11-22 21:36:15 +0100

    format bash

--- updateWorkspaces.sh
+++ updateWorkspaces.sh
@@ -4,7 +4,7 @@
 
 # Inclusions des fonctions communes.
 if [ -z ${FUNCTIONS_INC} ] ; then
-	. functions.sh
+    . functions.sh
 fi
 
 ####################
@@ -13,8 +13,8 @@ fi
 
 # Fix d'un bug concernant la gestion du cache GWS sur MacOS X
 if [ "$(uname)" == "Darwin" ] ; then
-	logWarn "BUGFIX ! Suppression du cache de GWS sur $(uname)"
-	find . -type f -name '.cache.gws' -exec rm -vf '{}' \;
+    logWarn "BUGFIX ! Suppression du cache de GWS sur $(uname)"
+    find . -type f -name '.cache.gws' -exec rm -vf '{}' \;
 fi
 
 logInfo "Mise a jour du workspace"
@@ -26,86 +26,86 @@ installHooks
 
 # Liste des périmètres obsolètes
 for r in aws proto saisie gendoc-templates v5 retd dev-tools ; do
-	if [ -d "${r}" ] ; then
-  		logWarn "${LRED}/!\\/!\\/!\\ Removed perimeter ${r} /!\\/!\\/!\\ ${RESTORE}"
-  		rm -Rf $r
-	fi
+    if [ -d "${r}" ] ; then
+        logWarn "${LRED}/!\\/!\\/!\\ Removed perimeter ${r} /!\\/!\\/!\\ ${RESTORE}"
+        rm -Rf $r
+    fi
 done
 
 # Renommage inopiné
 if [ -d amiante-voirie/voirie-parent ] ; then
-	logWarn "Renommage de voirie-parent -> amiante-voirie-parent"
-	mv amiante-voirie/voirie-parent amiante-voirie/amiante-voirie-parent
+    logWarn "Renommage de voirie-parent -> amiante-voirie-parent"
+    mv amiante-voirie/voirie-parent amiante-voirie/amiante-voirie-parent
 fi
 if [ -d declaration-chantier/declaration-chantier-parent ] ; then
-	logwarn "Renommage de declaration-chantier-parent -> pradictio-parent"
-	mv declaration-chantier/declaration-chantier-parent declaration-chantier/pradictio-parent
+    logwarn "Renommage de declaration-chantier-parent -> pradictio-parent"
+    mv declaration-chantier/declaration-chantier-parent declaration-chantier/pradictio-parent
 fi
 if [ -d declaration-chantier/declaration-chantier-multiplexeur ] ; then
-  logwarn "Renommage de declaration-chantier-parent -> pradictio-parent"
-  mv declaration-chantier/declaration-chantier-multiplexeur declaration-chantier/pradictio-multiplexeur
+    logwarn "Renommage de declaration-chantier-parent -> pradictio-parent"
+    mv declaration-chantier/declaration-chantier-multiplexeur declaration-chantier/pradictio-multiplexeur
 fi
 if [ -d declaration-chantier/messaging-declaration-chantier-schemas ] ; then
-  logwarn "Renommage de messaging-declaration-chantier-schemas -> messaging-pradictio-schemas"
-  mv declaration-chantier/messaging-declaration-chantier-schemas declaration-chantier/messaging-pradictio-schemas
+    logwarn "Renommage de messaging-declaration-chantier-schemas -> messaging-pradictio-schemas"
+    mv declaration-chantier/messaging-declaration-chantier-schemas declaration-chantier/messaging-pradictio-schemas
 fi
 if [ -d declaration-chantier ] ; then
-	logwarn "Renommage de declaration-chantier -> pradictio"
-	mv declaration-chantier pradictio
+    logwarn "Renommage de declaration-chantier -> pradictio"
+    mv declaration-chantier pradictio
 fi
 
 logInfo "Scan du worksapce a synchroniser"
 for rootCtx in * ; do
-  logInfo "Check synchro $rootCtx"
-	if [ -d $rootCtx ] && [ -f $rootCtx/.projects.gws ] ; then
-		cd $rootCtx
+    logInfo "Check synchro $rootCtx"
+    if [ -d $rootCtx ] && [ -f $rootCtx/.projects.gws ] ; then
+        cd $rootCtx
 
-		ALREADY_CLONED=`ls | wc -l`
-		if [ "$ALREADY_CLONED" -ne "0" ] ; then
-		  logInfo "Contenu a synchroniser détecté $PWD"
-		  gws update --only-changes
+        ALREADY_CLONED=`ls | wc -l`
+        if [ "$ALREADY_CLONED" -ne "0" ] ; then
+            logInfo "Contenu a synchroniser détecté $PWD"
+            gws update --only-changes
 
-		  for repo in * ; do
-				if [ ! -d $repo ] ; then
-					continue
-				fi
+            for repo in * ; do
+                if [ ! -d $repo ] ; then
+                    continue
+                fi
 
-		    # Contrôle que le repo n'as pas été déplacer
-		    grep $repo.git .projects.gws > /dev/null
-		    if [ "$?" -ne "0" ] ; then
-  		    echo -e "${LRED}$repo${RESTORE}: ${LRED} /!\\/!\\/!\\ Removed from GWS management /!\\/!\\/!\\ ${RESTORE}"
-		      rm -Rf $repo
+                # Contrôle que le repo n'as pas été déplacer
+                grep $repo.git .projects.gws > /dev/null
+                if [ "$?" -ne "0" ] ; then
+                    echo -e "${LRED}$repo${RESTORE}: ${LRED} /!\\/!\\/!\\ Removed from GWS management /!\\/!\\/!\\ ${RESTORE}"
+                    rm -Rf $repo
+                fi
+
+                if [ -d $repo/.git ] ; then
+                    cd $repo
+                    fetchGit
+                    cd ..
+                fi
+            done;
+
+            gws ff --only-changes
+
+            if [ -f .mu_repo ] ; then
+                rm -f .mu_repo
+            fi
+            echo serial=True > .mu_repo
+
+            for repo in * ; do
+                if [ -d $repo/.git ] ; then
+                    addToMu $repo
+
+                    cd $repo
+                    installHooks
+                    cd ..
+                fi
+            done;
         fi
 
-		    if [ -d $repo/.git ] ; then
-          cd $repo
-    			fetchGit
-    			cd ..
-		    fi
-		  done;
-
-		  gws ff --only-changes
-
-			if [ -f .mu_repo ] ; then
-		  	rm -f .mu_repo
-			fi
-		  echo serial=True > .mu_repo
-
-			for repo in * ; do
-		    if [ -d $repo/.git ] ; then
-					addToMu $repo
-
-					cd $repo
-					installHooks
-					cd ..
-		    fi
-		  done;
-		fi
-
-		cd ..
-	else
-	  logWarn "$rootCtx n'est pas a synchroniser"
-	fi
+    cd ..
+    else
+        logWarn "$rootCtx n'est pas a synchroniser"
+    fi
 done
 
 echo "$(date '+%d-%m-%Y %H:%M:%S')" > .lastUpdated
